1f06f2a6a9bda979c34cb284612c8458debaf0cc
Marley Barrett <marleyhansenbarrett@icloud.com>
2024-10-19 20:39:28 -0500

    basic cleanup

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,12 @@ export default async function Index() {
       <Hero />
       <main className="flex-1 flex flex-col gap-6 px-4">
         <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+        <p>
+          1. Invite your students to add to their profile
+        </p>
+        <p>
+          2. Plug in your class info, learning outcomes and more to start making projects!
+        </p>
       </main>
     </>
   );
