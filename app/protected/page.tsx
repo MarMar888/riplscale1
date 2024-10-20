bc82a6407b0613ea3c6fb56d43c1d4d9dbbee9ec
Marley Barrett <marleyhansenbarrett@icloud.com>
2024-10-19 21:10:12 -0500

    adjust

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -22,33 +22,20 @@ export default async function ProtectedPage() {
 
   return (
     <div className="flex-1 w-full flex flex-col gap-12">
-      <div className="w-full">
+      <div className="w-full gap-12">
 
+
+        <Label htmlFor="ClassName">Class Name</Label>
         <Input name="ClassName" placeholder="AP Statistics" required />
+        <Label htmlFor="ClassName">Grade Level</Label>
         <Input name="GradeLevel" placeholder="12th Grade" required />
+        <Label htmlFor="ClassName">Current Learning Objectives / Mastery Target</Label>
         <Input name="clos" placeholder="Chi Squared Test" required />
         <SubmitButton pendingText="Creating Projects" 
         // formAction={}
         >
           Create Projects
         </SubmitButton>
-
-
-        <div className="bg-accent text-sm p-3 px-5 rounded-md text-foreground flex gap-3 items-center">
-          <InfoIcon size="16" strokeWidth={2} />
-          This is a protected page that you can only see as an authenticated
-          user
-        </div>
-      </div>
-      <div className="flex flex-col gap-2 items-start">
-        <h2 className="font-bold text-2xl mb-4">Your user details</h2>
-        <pre className="text-xs font-mono p-3 rounded border max-h-32 overflow-auto">
-          {JSON.stringify(user, null, 2)}
-        </pre>
-      </div>
-      <div>
-        <h2 className="font-bold text-2xl mb-4">Next steps</h2>
-        <FetchDataSteps />
       </div>
     </div>
   );
