a6b3ff972234fc6ec8b481a9d82e5ba18fc335f7
Marley Barrett <marleyhansenbarrett@icloud.com>
2024-10-21 14:30:27 -0500

    yeah!!!

--- app/(auth-pages)/sign-in/page.tsx
+++ app/(auth-pages)/sign-in/page.tsx
@@ -1,11 +1,19 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
 import { signInAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
+import { FormMessage } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 
-export default function Login({ searchParams }: { searchParams: Message }) {
+export default function Login() {
+  const searchParams = useSearchParams();
+  const message = {
+    success: searchParams.get("success") || "",
+  };
+
   return (
     <form className="flex-1 flex flex-col min-w-64">
       <h1 className="text-2xl font-medium">Sign in</h1>
@@ -36,7 +44,7 @@ export default function Login({ searchParams }: { searchParams: Message }) {
         <SubmitButton pendingText="Signing In..." formAction={signInAction}>
           Sign in
         </SubmitButton>
-        <FormMessage message={searchParams} />
+        <FormMessage message={message} />
       </div>
     </form>
   );
