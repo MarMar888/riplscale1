8890d22d8a0dbffc2a07dac3438e974050c649a1
Marley Barrett <marleyhansenbarrett@icloud.com>
2024-10-22 08:28:44 -0500

    page

--- app/(auth-pages)/sign-up/page.tsx
+++ app/(auth-pages)/sign-up/page.tsx
@@ -1,56 +1,63 @@
+"use client";
+
 import { signUpAction } from "@/app/actions";
-import { FormMessage, Message } from "@/components/form-message";
+import { FormMessage } from "@/components/form-message";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
 import { SmtpMessage } from "../smtp-message";
+import { useSearchParams } from "next/navigation";
 
-export default function Signup({ searchParams }: { searchParams: Message }) {
-  if ("message" in searchParams) {
-    return (
-      <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
-        <FormMessage message={searchParams} />
-      </div>
-    );
-  }
+export default function Signup() {
+  const searchParams = useSearchParams();
+  const message = searchParams.get("message");
 
   return (
     <>
-      <form className="flex flex-col min-w-64 max-w-64 mx-auto">
-        <h1 className="text-2xl font-medium">Sign up</h1>
-        <p className="text-sm text text-foreground">
-          Already have an account?{" "}
-          <Link className="text-primary font-medium underline" href="/sign-in">
-            Sign in
-          </Link>
-        </p>
-        <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-          <Label htmlFor="email">Email</Label>
-          <Input name="email" placeholder="you@example.com" required />
-
-          <Label htmlFor="password">Password</Label>
-          <Input
-            type="password"
-            name="password"
-            placeholder="Your password"
-            minLength={6}
-            required
-          />
-
-          <Label htmlFor="classroom_name">Classroom Name</Label>
-          <Input
-            name="classroom_name"
-            placeholder="Enter your classroom name"
-            required
-          />
-
-          <SubmitButton formAction={signUpAction} pendingText="Signing up...">
-            Sign up
-          </SubmitButton>
-          <FormMessage message={searchParams} />
+      {message ? (
+        <div className="w-full flex-1 flex items-center h-screen sm:max-w-md justify-center gap-2 p-4">
+          <FormMessage message={{ success: message }} />
         </div>
-      </form>
+      ) : (
+        <form className="flex flex-col min-w-64 max-w-64 mx-auto">
+          <h1 className="text-2xl font-medium">Sign up</h1>
+          <p className="text-sm text text-foreground">
+            Already have an account?{" "}
+            <Link
+              className="text-primary font-medium underline"
+              href="/sign-in"
+            >
+              Sign in
+            </Link>
+          </p>
+          <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+            <Label htmlFor="email">Email</Label>
+            <Input name="email" placeholder="you@example.com" required />
+
+            <Label htmlFor="password">Password</Label>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Your password"
+              minLength={6}
+              required
+            />
+
+            <Label htmlFor="classroom_name">Classroom Name</Label>
+            <Input
+              name="classroom_name"
+              placeholder="Enter your classroom name"
+              required
+            />
+
+            <SubmitButton formAction={signUpAction} pendingText="Signing up...">
+              Sign up
+            </SubmitButton>
+            {message && <FormMessage message={{ success: message }} />}
+          </div>
+        </form>
+      )}
       <SmtpMessage />
     </>
   );
